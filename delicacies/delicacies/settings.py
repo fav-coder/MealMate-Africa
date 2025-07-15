56e478cb36168df1c0e77c722c2d4e14b4f1aee8
fav-coder <favoursangala046@gmail.com>
2025-07-16 00:36:12 +0300

    Set DEBUG to False for production

--- delicacies/delicacies/settings.py
+++ delicacies/delicacies/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i4z$!mvim0qz09mrva5cbl)!f%!zz%0zlz+ws1u0e(5*$5pyki'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = ['mealmate-africa-1.onrender.com', 'localhost', '127.0.0.1']
 
